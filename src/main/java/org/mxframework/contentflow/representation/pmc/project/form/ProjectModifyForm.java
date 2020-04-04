8512799a34f088d17903a29d0f872ce413c1b649
rottokaka <rottokaka@gmail.com>
2020-04-04 19:28:18 +0800

    fix: 项目无法无法修改名称 closes issuses #1

--- src/main/java/org/mxframework/contentflow/representation/pmc/project/form/ProjectModifyForm.java
+++ src/main/java/org/mxframework/contentflow/representation/pmc/project/form/ProjectModifyForm.java
@@ -13,20 +13,20 @@ import javax.validation.constraints.Size;
 @Data
 public class ProjectModifyForm {
 
-    @NotNull(message = "话题名称不能为空")
-    @Size(min = 2, max = 10, message = "话题名称字符串不能超出范围‘2~10’")
+    @NotNull(message = "项目名称不能为空")
+    @Size(min = 2, max = 20, message = "项目名称字符串不能超出范围‘2~20’")
     private String name;
 
-    @Size(max = 1000, message = "话题描述字符串长度不能超过1000")
+    @Size(max = 1000, message = "项目描述字符串长度不能超过1000")
     private String description;
 
-    @Size(max = 100, message = "话题网站地址长度不能超过100")
+    @Size(max = 100, message = "项目网站地址长度不能超过100")
     private String website;
 
     /**
      * 默认为“0”
      */
-    @NotNull(message = "话题上级话题不能为空")
+    @NotNull(message = "项目上级项目不能为空")
     private String aboveProjectId;
 
 }
